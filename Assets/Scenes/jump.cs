b07dbfbe7baf8a34390bbb06d89af5f76c8a6e19
Nicolás Flores <newabstraction1@gmail.com>
2025-05-06 14:39:33 -0400

    Revert "Update jump.cs"
    This reverts commit 97163db57e6f2c0f4ca4da6c9204eac64dfbf816.

--- Assets/Scenes/jump.cs
+++ Assets/Scenes/jump.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class jump : MonoBehaviour
+{
+    // Start is called before the first frame update
     void Start()
     {
-       
-    
+        transform.Translate(Vector2.up * Time.deltaTime);
     }
 
     // Update is called once per frame
