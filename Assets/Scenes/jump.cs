97163db57e6f2c0f4ca4da6c9204eac64dfbf816
Nicolás Flores <newabstraction1@gmail.com>
2025-05-06 14:39:16 -0400

    Update jump.cs

--- Assets/Scenes/jump.cs
+++ Assets/Scenes/jump.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jump : MonoBehaviour
-{
-    // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(Vector2.up * Time.deltaTime);
+       
+    
     }
 
     // Update is called once per frame
